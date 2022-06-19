5720e680908296eebc3210d2025051ba65236f1a
AnotherTwinkle <rfijaj003@gmail.com>
2022-06-19 20:37:00 +0600

    Add solution for problem `A - Optimal Path` Link: https://codeforces.com/contest/1700/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1700/submission/161234903

---
+++ codeforces/1700/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+#define TestCase() int ts; cin >> ts; while(ts--)
+#define printvec(a) for(auto& i : (a)) cout << i << " "; cout << endl
+#define printpair(a) cout << "(" << (a).ff  << " "<< (a).ss << ")" << endl
+#define returncout(s) cout << ((s)) << endl; return
+
+#define all(v) (v).begin(), (v).end()
+#define sz(x) (int)(x).size()
+#define ff first
+#define ss second
+
+const long long llzero = 0;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	setIO();
+	TestCase() {
+		ll n, m; cin >> n >> m; cout << m*(m+1)/2+ ((n*(n+1)/2)-1)*m << endl;
+	}
+}
