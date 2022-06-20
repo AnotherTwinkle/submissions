cf5925c6799b992438f68eff8414ced27667e2ad
AnotherTwinkle <rfijaj003@gmail.com>
2022-06-20 23:41:00 +0600

    Add solution for problem `B - Card Trick` Link: https://codeforces.com/contest/1681/problem/B Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1681/submission/161330307

---
+++ codeforces/1681/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+#define TestCase() int ts; cin >> ts; while(ts--)
+#define printvec(a) for(auto& i : (a)) cout << i << " "; cout << endl
+#define printpair(a) cout << "(" << (a).ff  << " "<< (a).ss << ")" << endl
+#define returncout(s) cout << ((s)) << endl; return
+
+#define all(v) (v).begin(), (v).end()
+#define sz(x) (int)(x).size()
+#define ff first
+#define ss second
+
+const long long llzero = 0;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	setIO();
+	TestCase() {
+		int n; cin >> n; vector<int> c(n);
+		for(int i = 0; i < n; i++) {cin >> c[i];}
+		int p = 0; int m; cin >> m;
+		while(m--) {
+			int x; cin >> x; p+=x;
+			if (p > n-1) p = p - n;
+		}
+		cout << c[p] << endl;
+	}
+}
