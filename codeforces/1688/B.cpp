d033d04801dd58762748d22278fa93c52289d6d8
AnotherTwinkle <rfijaj003@gmail.com>
2022-06-24 00:59:00 +0600

    Add solution for problem `B - Patchouli's Magical Talisman` Link: https://codeforces.com/contest/1688/problem/B Tags: bitmasks, constructive algorithms, greedy, sortings, *800 Ref: https://codeforces.com/contest/1688/submission/161584263

---
+++ codeforces/1688/B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+#define TestCase() int ts; cin >> ts; while(ts--)
+#define printvec(a) for(auto& i : (a)) cout << i << " "; cout << endl
+#define printpair(a) cout << "(" << (a).ff  << " "<< (a).ss << ")" << endl
+#define returncout(s) cout << ((s)) << endl; return
+#define vecinput(x, v) for(int fjj = 0; fjj < (x); fjj++) cin >> (v)[fjj]
+
+#define all(v) (v).begin(), (v).end()
+#define sz(x) (int)(x).size()
+#define ff first
+#define ss second
+#define ctz(x) (ll)__builtin_ctz((x))
+
+const long long llzero = 0;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	setIO();
+	TestCase() {
+		int n; cin >> n; vector<ll> e, o;
+		ll el = 10e17;
+		while(n--) {
+			ll x; cin >> x;
+			if(x%2==0) {
+				e.push_back(x);
+				el = min(ctz(x), el);
+			}
+			else o.push_back(x);
+		}
+		if(sz(o)) cout << sz(e) << endl;
+		else cout << el + (sz(e) - 1) << endl;
+	}
+}
