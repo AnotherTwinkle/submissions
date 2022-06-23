955a50ea1c4d2da0b86d8c694d838b08c6d38f42
AnotherTwinkle <rfijaj003@gmail.com>
2022-06-23 23:03:00 +0600

    Add solution for problem `A - Palindromic Indices` Link: https://codeforces.com/contest/1682/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1682/submission/161575847

---
+++ codeforces/1682/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+#define TestCase() int ts; cin >> ts; while(ts--)
+#define printvec(a) for(auto& i : (a)) cout << i << " "; cout << endl
+#define printpair(a) cout << "(" << (a).ff  << " "<< (a).ss << ")" << endl
+#define returncout(s) cout << ((s)) << endl; return
+#define vecinput(x, v) for(int fjj = 0; fjj < (x); fjj++) cin >> (v)[fjj]
+
+#define all(v) (v).begin(), (v).end()
+#define sz(x) (int)(x).size()
+#define ff first
+#define ss second
+
+const long long llzero = 0;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	setIO();
+	TestCase() {
+		int n; string s;
+		cin >> n >> s; int x = n/2;
+		int m = 0;
+		for(int i = x; i < n; i++) {if (s[i]==s[x]) {m++;} else {break;}}
+		for(int i = x - 1; i >= 0; i--) {if (s[i]==s[x]) {m++;} else {break;}}
+
+		cout << m << endl;
+	}
+}
