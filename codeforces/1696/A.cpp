5594ad40ad58a25e38c5a5d571d1194b248a5524
AnotherTwinkle <rfijaj003@gmail.com>
2022-06-25 21:10:00 +0600

    Add solution for problem `A - NIT orz!` Link: https://codeforces.com/contest/1696/problem/A Tags: bitmasks, greedy, *800 Ref: https://codeforces.com/contest/1696/submission/161762880

---
+++ codeforces/1696/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+#define TestCase() int ts; cin >> ts; while(ts--)
+#define printvec(a) for(auto& i : (a)) cout << i << " "; cout << endl
+#define printpair(a) cout << "(" << (a).ff  << " "<< (a).ss << ")" << endl
+#define returncout(s) cout << ((s)) << endl; return
+
+#define all(v) (v).begin(), (v).end()
+#define sz(x) (int)(x).size()
+#define ff first
+#define ss second
+
+const long long llzero = 0;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	setIO();
+	TestCase() {
+		int n, z; cin >> n >> z; vector<ll> v(n);
+		ll m = 0;
+		while(n--) {ll x; cin >> x; m = max(x|z, m);}
+		cout << m << endl;
+	}
+}
