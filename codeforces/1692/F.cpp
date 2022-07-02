7eef4d5b815ead6de1ce6bbd0da36c1da88c86d9
AnotherTwinkle <rfijaj003@gmail.com>
2022-07-03 01:51:00 +0600

    Add solution for problem `F - 3SUM` Link: https://codeforces.com/contest/1692/problem/F Tags: brute force, math, *1300 Ref: https://codeforces.com/contest/1692/submission/162583222

---
+++ codeforces/1692/F.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+void solve() {
+	int n;
+	std::cin >> n;
+	std::vector<int> freq(10, 0);
+	
+	for (int i = 0; i < n; i++) {
+		std::string x;
+		std::cin >> x;
+		int d = x.back() - '0';
+		int mx = (d == 1 ? 3 : 2);
+		freq[d] = std::min(mx, freq[d] + 1);
+	}
+
+	std::vector<int> v;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < freq[i]; j++) {
+			v.push_back(i);
+		}
+	}
+
+	int s = v.size();
+	for (int i = 0; i < s; i++) {
+		for (int j = 0; j < s; j++) {
+			for (int k = 0; k < s; k++) {
+				if (i == j || j == k || i == k) continue;
+				if ((v[i] + v[j] + v[k]) % 10 == 3) {
+					std::cout << "YES\n";
+					return;
+				}
+			}
+		}
+	}
+
+	std::cout << "NO\n";
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int t;
+	std::cin >> t;
+
+	while (t--) solve();
+}
