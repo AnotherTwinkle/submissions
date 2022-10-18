66925d43e09559c7388593014cdac396a8ac07ce
AnotherTwinkle <rfijaj003@gmail.com>
2022-10-18 14:00:00 +0600

    Add solution for problem `B - Box Fitting` Link: https://codeforces.com/contest/1498/problem/B Tags: binary search, bitmasks, data structures, greedy, *1300 Ref: https://codeforces.com/contest/1498/submission/176848145

--- codeforces/1498/B.cpp
+++ codeforces/1498/B.cpp
@@ -28,8 +28,8 @@ void solve() {
 }
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    // std::ios::sync_with_stdio(false);
+    // std::cin.tie(nullptr);
 
     int t;
     std::cin >> t;
