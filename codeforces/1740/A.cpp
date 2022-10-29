4c33863879dc5b6248838ac9bfec343f6eb1b48c
AnotherTwinkle <rfijaj003@gmail.com>
2022-10-29 20:26:00 +0600

    Add solution for problem `A - Factorise N+M` Link: https://codeforces.com/contest/1740/problem/A Tags: constructive algorithms, number theory, *800 Ref: https://codeforces.com/contest/1740/submission/178414870

---
+++ codeforces/1740/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using pair = std::array<int, 2>;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::cout << n << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
